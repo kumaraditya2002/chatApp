db205bebace01ac655628bafe535a7ed173a01fe
Aditya Kumar <adityakumar9006@gmail.com>
2020-12-07 20:17:44 +0530

    cors header resolved

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-const io = require('socket.io')(8000);
+const express = require('express');
+const http = require('http');
+
 const users = {};
+// const port=3000;
+// const host='localhost';
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res)=>{
+    req.status = 200;
+    res.sendFile('index.html',{root:__dirname});
+});
+
+const io = require("socket.io")(server, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            // "Access-Control-Allow-Headers": "Content-Type, application/json",
+            "Access-Control-Allow-Origin": '*',
+            // "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
 
 io.on('connection',socket => {
     socket.on('new-user-joined',name => {
@@ -16,3 +42,5 @@ io.on('connection',socket => {
         delete users[socket.id];
     });
 });
+
+server.listen(8000);
